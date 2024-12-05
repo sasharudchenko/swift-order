69ed1126d20d7aef7fb85a36d063984a123fe189
Alexandr <sasharudchenko@yandex.ru>
2024-12-05 18:40:18 +0500

    Пустая коробка

---
+++ delivery-service/src/test/java/ru/rudchenko/deliveryservice/TestDeliveryServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.rudchenko.deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(DeliveryServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
